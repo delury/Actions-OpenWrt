74094a400f7193d9c2c312177906f967e176dd5c
delury <54013363+delury@users.noreply.github.com>
2020-12-26 21:36:45 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -15,3 +15,5 @@
 
 # Add a feed source
 #sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
+git clone https://github.com/rufengsuixing/luci-app-adguardhome package/luci-app-adguardhome
+git clone https://github.com/esirplayground/luci-app-poweroff package/luci-app-poweroff
